0d89f44b80c1aaa6012ffd02b0ab3d0bf94f947f
vanita22 <vanitayrafita2004@gmail.com>
2017-06-29 02:07:44 -0300

    actualizando

--- js/main.js
+++ js/main.js
@@ -3,13 +3,14 @@ $(document).ready(function(){
 	var tarea = function guardar(){
 		localStorage.tarea = document.getElementById("tarea").value;
 	}
-	
-	$(tarea).keypress(function() {
+		
+	$(tarea).keypress(function(){
 
 		if(tarea == ""){
 			alert("no debes dejar este campo vacio");
 		}else {	         		
       		$('ul').after('<li>' +'<label for="list">' + tarea+ '</label>' + '</li>');
       	}	
-	});
+    });
+	
 })
